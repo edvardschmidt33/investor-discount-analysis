70d8e2c93d31aaf1d865f64a299d2f32e08624ce
edvardschmidt33 <edvardschmidt3333@gmail.com>
2025-10-14 15:42:36 -0600

    refined plot.py

--- data/plots.py
+++ data/plots.py
@@ -2,12 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-### Preprocessing Investor ###
+### Preprocessing STOCK ###
+
+STOCK = 'Investor'
+
 exclude_cols = ['Rabatt/Premie', 'Genomsnittsrabatt senaste 100 handelsdagarna', 'Nuvarande rabatt minus snitt', 'Avkastning 200 handelsdagar', 'OMX Date']
 
 df = pd.read_csv("data/Investor.csv", usecols=lambda col: col not in exclude_cols)
 
 numeric_cols = ['PRIS', 'SUBSTANSVÄRDE', 'BERÄKNAT_SUBSTANSVÄRDE', 'Index Value', 'Avkastning OMXS#=']
+
+
+
 
 for col in numeric_cols:
    df[col] = (
@@ -21,8 +27,8 @@ for col in numeric_cols:
 
 df['Investor Date'] = pd.to_datetime(df['Investor Date'], errors='coerce')
 
-print(df.head())
-print(df.dtypes)
+# print(df.head())
+# print(df.dtypes)
 
 
 cutoff_date = '2024-12-11'
@@ -35,36 +41,58 @@ num_subset = df[numeric_cols]
 
 # Rabatt/premie: Ber.substansvärde / pris
 
-df['RABATT/PREMIE'] = df['BERÄKNAT_SUBSTANSVÄRDE'] / df['PRIS']
+df['RABATT/PREMIE'] = (df['BERÄKNAT_SUBSTANSVÄRDE'] - df['PRIS']) / df['PRIS']
 df['AVKASTNING'] =(df['PRIS'].shift(-200) - df['PRIS']) / df['PRIS']
 
 df['AVKASTNING - OMXS'] = df['AVKASTNING'] - df['Avkastning OMXS#=']*0.01
 
-print(df.head())
 
-# print(num_subset.corr())
-
-
-
-# test plot
-# plt.plot(df['Investor Date'], df['PRIS'], label = 'Investor')
-
+### CORRELATION PLOTS ###
 
 plt.scatter(df['RABATT/PREMIE'], df['AVKASTNING - OMXS'])
 plt.axhline(0, color='red', linestyle='--', linewidth=1)
+plt.xlabel('Discount/Premium')
+plt.ylabel('Return rate adjusted for OMXS')
+plt.title(f'Discount/Premium against adjusted return rate for {STOCK}')
 plt.show()
 
 
+plt.scatter(df['RABATT/PREMIE'], df['AVKASTNING'])
+plt.axhline(0, color='red', linestyle='--', linewidth=1)
+plt.xlabel('Discount/Premium')
+plt.ylabel('Return rate')
+plt.title(f'Discount/Premium against return rate for {STOCK}')
 plt.show()
+
+### BOXPLOTS ###
+
+plt.boxplot(df['AVKASTNING'])
+plt.title(f'Return rate boxplot for {STOCK}')
+plt.show()
+
+plt.show()
+plt.boxplot(df['AVKASTNING - OMXS'])
+plt.title(f'Adjusted return rate boxplot for {STOCK}')
+plt.show()
+
+
 plt.boxplot(df['RABATT/PREMIE'])
+plt.title(f'Discount/premium boxplot for {STOCK}')
 plt.show()
 
+
+### HISTOGRAMS ###
 
 plt.hist(df['RABATT/PREMIE'].dropna(), bins=30, color='skyblue', edgecolor='black')
-
-plt.title('Histogram of Discount/Premium')
-plt.xlabel('Discount / Premium (%)')
+plt.xlabel('Discount / Premium ')
 plt.ylabel('Frequency')
 plt.grid(True, linestyle='--', alpha=0.6)
+plt.title(f'Discount/Premium histogram for {STOCK}')
 plt.show()
 
+### Plots over time ###
+
+plt.plot(df['Investor Date'], df['RABATT/PREMIE'])
+plt.xlabel('Date')
+plt.ylabel('Discount/Premium')
+plt.show()
