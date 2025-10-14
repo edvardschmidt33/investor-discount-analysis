5f189443aabc73a1cd2050a50a3f162119e45ee6
edvardschmidt33 <edvardschmidt3333@gmail.com>
2025-10-14 13:22:48 -0600

    fixing preprocess

--- data/plots.py
+++ data/plots.py
@@ -1,13 +1,26 @@
 import pandas as pd
 
 
+### Preprocessing Investor ###
+exclude_cols = ['Rabatt/Premie', 'Genomsnittsrabatt senaste 100 handelsdagarna', 'Nuvarande rabatt minus snitt', 'Avkastning 200 handelsdagar']
 
-df = pd.read_csv("data/Investor.csv")
+df = pd.read_csv("data/Investor.csv", usecols=lambda col: col not in exclude_cols)
+
+numeric_cols = ['PRIS', 'SUBSTANSVÄRDE', 'BERÄKNAT_SUBSTANSVÄRDE', 'Index Value', 'Avkastning OMXS#=']
+
+for col in numeric_cols:
+   df[col] = (
+        df[col]
+        .astype(str)                 # make sure it's a string
+        .str.replace(' ', '', regex=False)  # remove spaces inside numbers
+        .str.replace(',', '.', regex=False) # replace decimal commas with dots
+        .pipe(pd.to_numeric, errors='coerce')  # safely convert to float
+    )
 
 
-for col in df.columns:
-    if df[col].dtype == 'object':
-        df[col] = df[col].str.replace(',', '.')
+
 
 print(df.head())
 print(df.dtypes)
+
+
